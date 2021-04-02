4807618b44c51f43f7dd7b8aa074341defc2591a
rebalint <rebalint@gmail.com>
2021-04-02 12:33:54 +0200

    Removed even more garbage

--- cmake-build-debug/ExternalIncludeFiles/libjpeg-turbo/build/CMakeFiles/jpeglib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libjpeglib.a"
-  "libjpeglib.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/jpeglib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
